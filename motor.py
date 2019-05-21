31542f3b4f7ab7cc41f3ddf266c4d1fa18a9d8aa
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-22 00:41:00 +0100

    Fixed both code for both motor running together

--- motor.py
+++ motor.py
@@ -9,9 +9,9 @@ Motor1A = 27
 Motor1B = 24
 Motor1Enable = 5
 
-Motor1A = 0
-Motor1B = 2
-Motor1Enable = 3
+Motor2A = 18
+Motor2B = 13
+Motor2Enable = 25
 
 
 '''Below is the GPIO setup'''
@@ -19,18 +19,25 @@ GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1Enable, GPIO.OUT)
 
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
+GPIO.setup(Motor2Enable, GPIO.OUT)
+
 '''Now we turn the motor on and send power to terminals'''
-GPIO.output(Motor1A, GPIO.LOW)
-GPIO.output(Motor1B, GPIO.HIGH)
-GPIO.output(Motor1Enable, GPIO.HIGH)
-
-sleep(3)
-
-GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1A, GPIO.HIGH)
+GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1Enable, GPIO.HIGH)
+
+GPIO.output(Motor2A, GPIO.HIGH)
+GPIO.output(Motor2B, GPIO.LOW)
+GPIO.output(Motor2Enable, GPIO.HIGH)
 
 sleep(3)
 
 GPIO.output(Motor1Enable, GPIO.LOW)
+GPIO.output(Motor2Enable, GPIO.LOW)
+
 GPIO.cleanup()
+
+'''GIPO Pins Used'''
+'''Refer to the documentation for PIN allocation of each motor'''
