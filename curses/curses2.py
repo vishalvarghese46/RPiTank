140f5a9957a9d2a430ca0d2e92352fafdb1a75d2
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-05-27 23:12:06 +0100

    Printing text in the middle of the screen

--- curses/curses2.py
+++ curses/curses2.py
@@ -3,3 +3,16 @@ from time import sleep
 
 def main(stdscr):
     curses.curs_set(0)
+    h, w = stdscr.getmaxyx()
+    text = "Hello, World!"
+
+    x = w//2 - len(text)//2;
+    y = h//2
+
+    stdscr.addstr(y,x, text)
+
+    stdscr.refresh()
+    sleep(3)
+curses.wrapper(main)
+
+
