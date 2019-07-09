512769cec16b12bd78e01f5985aefacdb4c35afa
Vishal Varghese <vishalvarghese46@gmail.com>
2019-07-09 23:21:29 +0100

    Working on the camera server

--- camera.py
+++ camera.py
@@ -13,6 +13,25 @@ def capture():
     left_button.when_released = camera.stop_preview
     right_button.when_pressed = capture
     pause()
+class SplitFrames(object):
+    def __init__(self, connection):
+        self.connection = connection
+        self.stream = io.BytesIO()
+        self.count = 0
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # Start of new frame; send the old one's length
+            # then the data
+            size = self.stream.tell()
+            if size > 0:
+                self.connection.write(struct.pack('<L', size))
+                self.connection.flush()
+                self.stream.seek(0)
+                self.connection.write(self.stream.read(size))
+                self.count += 1
+                self.stream.seek(0)
+        self.stream.write(buf)
 
 
 
