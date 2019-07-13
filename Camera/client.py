cb5d4f46230c6024ba8825a31e1791c7d5e45fe3
Vishal Varghese <vishalvarghese46@gmail.com>
2019-07-13 21:35:25 +0100

    finishing vlc camera server

--- Camera/client.py
+++ Camera/client.py
@@ -4,14 +4,13 @@ import picamera
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
-client_socket = socket.socket()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('192.168.1.67', 1234))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
 try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
+    camera = picamera.PiCamera(resolution=(640, 480))
     camera.framerate = 24
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
