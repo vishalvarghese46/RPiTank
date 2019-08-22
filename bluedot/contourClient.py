fa5835ff03069e3254bdfeff32ee3e277eaa318c
Vishal Varghese <vishalvarghese46@gmail.com>
2019-08-22 22:31:47 +0100

    Starting works on the line following robot

---
+++ bluedot/contourClient.py
@@ -0,0 +1,40 @@
+import io
+import socket
+import struct
+import time
+import picamera
+
+# create socket and bind host
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.1.67', 8000))
+connection = client_socket.makefile('wb')
+
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)  # pi camera resolution (640x480)
+        camera.framerate = 15  # 15 frames/sec
+        time.sleep(2)  # give 2 secs for camera to initilize
+        start = time.time()
+        stream = io.BytesIO()
+
+        # send jpeg format video stream
+        for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            if time.time() - start > 600:
+                break
+            stream.seek(0)
+            stream.truncate()
+    connection.write(struct.pack('<L', 0))
+
+
+    msg = client_socket.recv(1024)
+    msg = msg.decode('utf-8')
+    if msg == '1':
+        print('left')
+
+finally:
+    connection.close()
+    client_socket.close()
---
+++ lineRobot/lineRobotServer.py
@@ -0,0 +1,85 @@
+import numpy as np
+import cv2
+import socket
+
+
+class videoStreaming(object):
+    def __init__(self, host, port):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((host, port))
+        self.server_socket.listen(0)
+        self.connection, self.client_address = self.server_socket.accept()
+        self.connectionBuff = self.connection.makefile('rb')
+        self.host_name = socket.gethostname()
+        self.host_ip = socket.gethostbyname(self.host_name)
+        self.streaming()
+
+    def streaming(self):
+
+        try:
+            print(f'Host: {self.host_name}, {self.host_ip}')
+            print(f'Connection from: {self.client_address}')
+            print("Streaming...")
+            print("Press 'q' to exit")
+
+            # need bytes here
+            stream_bytes = b' '
+            while True:
+                stream_bytes += self.connectionBuff.read(1024)
+                first = stream_bytes.find(b'\xff\xd8')
+                last = stream_bytes.find(b'\xff\xd9')
+                if first != -1 and last != -1:
+                    jpg = stream_bytes[first:last + 2]
+                    stream_bytes = stream_bytes[last + 2:]
+                    image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    cv2.imshow('image', image)
+
+                    grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                    blur = cv2.GaussianBlur(grey_img, (5,5), 0)
+                    ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
+
+                    contours, hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+
+                    if len(contours)>0:
+                        c = max(contours, key=cv2.contourArea)
+                        M = cv2.moments(c)
+
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+
+                        cv2.line(image, (cx, 0), (cx, 720), (255, 0, 0), 1)
+                        cv2.line(image, (0, cy), (1280, cy), (255, 0, 0), 1)
+
+                        cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
+                        self.robot(cx)
+
+                    else:
+                        print("Can't see shit dawg!")
+
+                    cv2.imshow('Frame', image)
+
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+        finally:
+            self.connection.close()
+            self.server_socket.close()
+
+    def robot(self, cx):
+        centroidx = cx
+        if centroidx >= 120:
+            self.connection.send(bytes("1", 'utf-8'))
+            print("1 - Forward")
+        elif centroidx < 120 and cx > 50:
+            self.connection.send(bytes("2", "utf-8"))
+            print("2 - Left")
+        elif centroidx <= 50:
+            self.connection.send(bytes("3", "utf-8"))
+            print("3 - right")
+        else:
+            print("Trouble!")
+
+if __name__ == '__main__':
+    # host, port
+    h, p = "192.168.1.67", 8000
+    videoStreaming(h, p)
